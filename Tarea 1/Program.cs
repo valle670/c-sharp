b279decc562e35a8ab1827ce33864e9da97c798c
valle670 <98921257+valle670@users.noreply.github.com>
2022-08-07 21:36:30 -0600

    Add files via upload

---
+++ Tarea 1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace inventario
+{
+    class Program
+    {
+        static string[,] productos = new string[5,3]
+        {
+            { "001", "iPhoneX", "0" },
+            { "002", "Laptop Dell", "5" },
+            { "003", "Monitor Samsung", "2" },
+            { "004", "Mouse", "100" },
+            { "005", "Headset", "25" },
+        };
+
+        static void listarProductos() {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Listado de Productos");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Codigo, Descripcion y Existencia");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(productos[i, 0] + " | " + productos[i, 1] + " | " + productos[i, 2]);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void movimientoInventario(string codigo, int cantidad, string tipoMovimiento) {
+            for (int i = 0; i < 5; i++)
+            {
+                if (productos[i, 0] == codigo) {
+                    if (tipoMovimiento == "+") {
+                        productos[i, 2] = (Int32.Parse(productos[i, 2]) + cantidad).ToString();
+                    } else {
+                        productos[i, 2] = (Int32.Parse(productos[i, 2]) - cantidad).ToString();
+                    }
+                }
+            }
+        }
+
+        static void IngresoDeInventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Ingreso de Productos al Inventario");
+            Console.WriteLine("-----------------------------------");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "+");
+        }
+        
+        static void AjustePositivodeInventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Ajuste Positivo de Productos del Inventario");
+            Console.WriteLine("-------------------------------------------");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "+");
+        }
+        static void SalidaDeInventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Salida del Productos del Inventario");
+            Console.WriteLine("-----------------------------------");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "-");
+        }
+        static void AjusteNegativodeInventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Ajuste Negativo de Productos del Inventario");
+            Console.WriteLine("-------------------------------------------");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "-");
+        }
+
+        
+        static void Main(string[] args)
+        {
+            string opcion = "";
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Sistema de Inventario");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Productos");
+                Console.WriteLine("2 - Ingreso de Inventario");
+                Console.WriteLine("3 - Salida de Inventario");
+                Console.WriteLine("4 - Ajuste Positivo de Inventario");
+                Console.WriteLine("5 - Ajuste Negativo de Inventario");
+                Console.WriteLine("0 - Salir");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1": 
+                        listarProductos();
+                        break;
+                    case "2":
+                        IngresoDeInventario();
+                        break;
+                    case "3": 
+                        SalidaDeInventario();
+                        break;
+                    case "4": 
+                        AjustePositivodeInventario();
+                        break;
+                    case "5": 
+                        AjusteNegativodeInventario();
+                        break;
+                    default:
+                    break;
+                }
+
+                if (opcion == "0") {
+                    break;
+                }
+            }            
+        }
+    }
+}
